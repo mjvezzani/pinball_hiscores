34e9ceb414b3befd2c2cfd2a9eef44287eb78619
Michael Vezzani <mjvezzani@gmail.com>
2016-02-29 19:26:10 -0500

    Hopefully fix hiscores create action

--- app/controllers/hi_scores_controller.rb
+++ app/controllers/hi_scores_controller.rb
@@ -2,14 +2,14 @@ class HiScoresController < ApplicationController
   def create
     @pin = Pin.find(params[:pin_id])
     @hi_score = @pin.hi_scores.build(hi_score_params)
-    @hi_score.user = current_user
+    @hi_score.user_id = current_user.id
 
     if @hi_score.save
       flash[:success] = "High score saved!"
-      redirect_to pin_path(@hi_score.pin)
+      redirect_to pin_path(@pin)
     else
       flash[:error] = "An error occurred"
-      redirect_to pin_path(@hi_score.pin)
+      redirect_to pin_path(@pin)
     end
   end
 
